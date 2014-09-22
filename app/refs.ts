c422cfedd7d51bf394164dea286ab4950c5f54c6
Luke Phillips <luke@femurdesign.com>
2014-09-22 09:46:05 +0100

    waa.d.ts to refs.ts

--- app/refs.ts
+++ app/refs.ts
@@ -2,3 +2,4 @@
 /// <reference path="./lib/fayde.drawing/Fayde.Drawing.d.ts" />
 /// <reference path="./lib/fayde.utils/Fayde.Utils.d.ts" />
 /// <reference path="./Tone.d.ts" />
+/// <reference path="./waa.d.ts" />
