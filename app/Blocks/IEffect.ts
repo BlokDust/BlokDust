a98365f308048e1e4fdaa499acffdf995b351208
Luke Phillips <luke@femurdesign.com>
2014-10-02 22:37:09 +0100

    Planning out Effect structure

--- app/Blocks/IEffect.ts
+++ app/Blocks/IEffect.ts
@@ -13,33 +13,120 @@ export = IEffect;
 
 
 
-// OUTPUT --> Effect Input --> Any other effect Input || Master
-
-// LFO connects to ModifiableAttributes
-// Envelope connects to ModifiableAttributes
-// Delay connects to Block Output
 
 /*
 
 SourceBlocks --> Generates Sound
-    Tone
-    Noise
 
-ModifierBlocks
+    Output: connects to EffectBlock.input || Master (default if not in range)
+
+    Types:
+        Tone
+             Params: Object --> AudioNode || Tone
+                Volume
+                    isModifiableByModifier: boolean
+                    Default
+                    Min
+                    Max
+                Pitch
+                    isModifiableByModifier: boolean
+                    Default
+                    Min
+                    Max
+                Waveform
+                    isModifiableByModifier: boolean
+                    Default
+                    Options: Array --> (if no min & max then only step choices in options array
+                        [sine, saw, square, triangle]
+
+        Noise
+             Params: Object --> AudioNode || Tone
+                Volume
+                    isModifiableByModifier: boolean
+                    Default
+                    Min
+                    Max
+                Waveform
+                    isModifiableByModifier: boolean
+                    Default
+                    Options: Array --> (if no min & max then only step choices in options array
+                        [pink, brown, white]
+
+        PreFab
+             Params: Object --> AudioNode || Tone
+                Volume
+                    isModifiableByModifier: boolean
+                    Default
+                    Min
+                    Max
+                PrefabSpecific??
+
+
+ModifierBlocks --> Modifies a parameter from a Source or an Effect (which ever is closest)
+
     Input: connects to SourceBlock.ModifiableAttribute || EffectBlock.ModifiableAttribute
 
     Types:
         LFO
+             Params
+                Frequency
+                    isModifiableByModifier: boolean
+                    Default
+                    Min
+                    Max
+                Depth
+                    isModifiableByModifier: boolean
+                    Default
+                    Min
+                    Max
+                Waveform
+                    isModifiableByModifier: boolean
+                    Default
+                    Options: Array --> (if no min & max then only step choices in options array
+                    [sine, triangle, square, saw]
+
         Envelope
+            Params
+                Attack
+                    Default
+                    Min
+                    Max
+                Decay
+                    Default
+                    Min
+                    Max
+                Sustain
+                    Default
+                    Min
+                    Max
+                Release
+                    Default
+                    Min
+                    Max
 
 
-EffectBlocks --> Connect from output of a SourceBlock to MasterOut
+EffectBlocks --> Effects A Sources Output
+
     Input: connects to SourceBlockOutput || EffectBlockOutput
-    Output: connects to MasterOutput || EffectBlockOutput
+    Output: connects to MasterOutput:default || EffectBlockOutput
+
     Types:
         Delay
+            Params
+                Feedback
+                    isModifiableByModifier: boolean
+                    Default
+                    Min
+                    Max
+                Time
+                    isModifiable... etc
+
+                WetLevel
+
         Distortion
+
         Phaser
+
         Chorus
 
 
