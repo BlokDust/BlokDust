65234e5c841aef5f231e9824863e0fddf65fff65
edsilv <e.silverton@gmail.com>
2014-12-08 14:55:00 +0000

    port to nullstone

--- app/DisplayObjectCollection.ts
+++ app/DisplayObjectCollection.ts
@@ -5,7 +5,7 @@ class DisplayObjectCollection<T extends IDisplayObject> extends Fayde.Collection
     constructor(){
         super();
 
-        this.CollectionChanged.Subscribe(() => {
+        this.CollectionChanged.on(() => {
             // update ZIndex properties
             for (var i = 0; i < this.Count; i++){
                 var obj = this.GetValueAt(i);
