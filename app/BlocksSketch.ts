501e3839b317cd880df62c9726b6f97b7509af02
Whitevinyl <neverest_songs@yahoo.co.uk>
2015-01-18 15:50:03 +0000

    test

--- app/BlocksSketch.ts
+++ app/BlocksSketch.ts
@@ -292,7 +292,7 @@ class BlocksSketch extends Grid {
                     (<any>e).args.Handled = true;
 
                     block.MouseDown();
-                    blockClick = false;
+                    blockClick = true;
                     this.SelectedBlock = block;
                     ParamTimeout = true;
                     setTimeout(function() {
