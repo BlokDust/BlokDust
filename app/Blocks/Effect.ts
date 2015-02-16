e43f5d04d3d3dad084b9d051bc85e231b0602a00
Luke Phillips <luke@femurdesign.com>
2015-02-16 18:12:49 +0000

    remove unnecessary code

--- app/Blocks/Effect.ts
+++ app/Blocks/Effect.ts
@@ -12,16 +12,9 @@ class Effect extends Block implements IEffect {
 
     Source: ISource;
     public Sources: ObservableCollection<ISource> = new ObservableCollection<ISource>();
-    public OldSources: ObservableCollection<ISource>;
-
-    //Params: ToneSettings;
 
     constructor(grid: Grid, position: Point) {
         super(grid, position);
-
-        this.Sources.CollectionChanged.on(() => {
-            this._OnSourcesChanged();
-        }, this);
 
         this.OpenParams();
     }
@@ -54,41 +47,6 @@ class Effect extends Block implements IEffect {
      */
     RemoveSource(source: ISource) {
         this.Sources.Remove(source);
-    }
-
-    private _OnSourcesChanged() {
-
-        // Detach effects in old collection.
-        //if (this.OldSources && this.OldSources.Count){
-        //    var oldEffects: IEffect[] = this.OldSources.ToArray();
-        //
-        //    for (var k = 0; k < oldEffects.length; k++) {
-        //        this._DetachEffect(oldEffects[k]);
-        //    }
-        //}
-
-        // List of connected effect blocks
-        var sources: ISource[] = this.Sources.ToArray();
-
-
-        // For each connected effect
-        //for (var i = 0; i < effects.length; i++) {
-        //
-        //    // Run Attach method for all effect blocks that need it
-        //    this._AttachEffect(effects[i]);
-        //
-        //    // If this is a post effect add to postEffect list
-        //    if (effects[i].Effect) {
-        //        postEffects.push(effects[i]);
-        //    }
-        //}
-
-        // Reorder the post effects chain
-        //this.UpdateEffectsChain(postEffects);
-
-        // Update list of Old Effects
-        this.OldSources = new ObservableCollection<ISource>();
-        this.OldSources.AddRange(this.Sources.ToArray());
     }
 }
 
