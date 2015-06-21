e21a5172dda0ec41527308e634fce7a7f2bedad0
edsilv <e.silverton@gmail.com>
2015-06-21 17:47:36 +0100

    fixed #77

--- app/Operations/SaveOperation.ts
+++ app/Operations/SaveOperation.ts
@@ -3,47 +3,56 @@ import IOperation = require("../Core/Operations/IOperation");
 class SaveOperation<String> implements IOperation
 {
     private _JSON: any;
+    private _LZMA: any;
 
     constructor(json: string, id?: string) {
         this._JSON = {
             "Id": (id) ? id : "",
             "Data": json
         };
+
+        this._LZMA = new LZMA("/lib/lzma/src/lzma_worker.js");
     }
 
-    //Compress(json: string){
-    //    // compress the json
-    //    var lzma = new LZMA();
-    //
-    //    lzma.compress(json, 5,
-    //        function(result) {
-    //            return result;
-    //        },
-    //        function(percent) {
-    //            console.log(percent);
-    //        });
-    //}
+    Compress(data: string): Promise<string>{
+
+        return new Promise((resolve) => {
+
+            this._LZMA.compress(data, 5,
+                function(result) {
+                    resolve(result.toString());
+                },
+                function(percent) {
+                    //console.log(percent);
+                }
+            );
+        });
+    }
 
     Do(): Promise<String> {
         var that = this;
 
         return new Promise((resolve) => {
 
-            var data = JSON.stringify(that._JSON);
+            //console.log(data);
 
-            console.log(data);
+            that.Compress(that._JSON.Data).then((compressed) => {
 
-            $.ajax(<JQueryAjaxSettings>{
-                url: "http://blokdust.azurewebsites.net/api/anonymousblobs",
-                type: 'POST',
-                crossDomain: true,
-                dataType: 'json',
-                contentType: 'application/json',
-                data: data
-            }).done(function(data){
-                resolve(data);
-            });
+                that._JSON.Data = compressed;
 
+                var data = JSON.stringify(that._JSON);
+
+                $.ajax(<JQueryAjaxSettings>{
+                    url: "http://blokdust.azurewebsites.net/api/anonymousblobs",
+                    type: 'POST',
+                    crossDomain: true,
+                    dataType: 'json',
+                    contentType: 'application/json',
+                    data: data
+                }).done(function(saved){
+                    resolve(saved);
+                });
+            })
         });
     }
 
