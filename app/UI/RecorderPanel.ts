563bfb039abbddd7b6b0feb441c5d5aa1e0cd73a
Whitevinyl <neverest_songs@yahoo.co.uk>
2015-07-12 13:27:34 +0100

    Share panel session aware

--- app/UI/RecorderPanel.ts
+++ app/UI/RecorderPanel.ts
@@ -1,3 +1,13 @@
 /**
  * Created by luketwyman on 12/07/2015.
  */
+import BlocksSketch = require("./../BlocksSketch");
+import IEffect = require("../Blocks/IEffect");
+import ISource = require("../Blocks/ISource");
+import IBlock = require("../Blocks/IBlock");
+
+class RecorderPanel {
+
+}
+
+export = RecorderPanel;
