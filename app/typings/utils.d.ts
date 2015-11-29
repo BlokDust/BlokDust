90fad6cbd7a0c91be01ddeb44a0bb805b585a3f7
Luke Phillips <luke@femurdesign.com>
2015-11-30 10:37:17 +1300

    Tone update - fixed #163 envelope issues

--- app/typings/utils.d.ts
+++ app/typings/utils.d.ts
@@ -1405,6 +1405,11 @@ declare module Utils {
     }
 }
 declare module Utils {
+    class Device {
+        static GetPixelRatio(ctx: CanvasRenderingContext2D): number;
+    }
+}
+declare module Utils {
     class Documents {
         static IsInIFrame(): boolean;
     }
@@ -1458,6 +1463,35 @@ declare module Utils {
     }
 }
 declare module Utils {
+}
+declare module Utils {
+    class Storage {
+        private static _memoryStorage;
+        static clear(storageType?: StorageType): void;
+        static clearExpired(storageType?: StorageType): void;
+        static get(key: string, storageType?: StorageType): StorageItem;
+        private static _isExpired(item);
+        static getItems(storageType?: StorageType): StorageItem[];
+        static remove(key: string, storageType?: StorageType): void;
+        static set(key: string, value: any, expirationSecs: number, storageType?: StorageType): StorageItem;
+    }
+}
+declare module Utils {
+    class StorageItem {
+        key: string;
+        value: any;
+        expiresAt: number;
+    }
+}
+declare module Utils {
+    class StorageType {
+        value: string;
+        static memory: StorageType;
+        static session: StorageType;
+        static local: StorageType;
+        constructor(value: string);
+        toString(): string;
+    }
 }
 declare module Utils {
     class Strings {
