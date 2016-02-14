4c5ce028d6e2bf110b7b8d7f399666102fa628a0
edsilv <e.silverton@gmail.com>
2016-02-14 14:12:50 +0000

    Revert "particle caching"
    This reverts commit 6f726d595681ccccdabe56e96af58515b1a0a4a3.

--- app/MainScene.ts
+++ app/MainScene.ts
@@ -40,7 +40,6 @@ import {ToolTip} from './UI/ToolTip';
 import {TrashCan} from './UI/TrashCan';
 import {Tutorial} from './UI/Tutorial';
 import {ZoomButtons} from './UI/ZoomButtons';
-import Canvas = etch.drawing.Canvas;
 
 declare var App: IApp;
 declare var OptionTimeout: boolean; //TODO: better way than using global? Needs to stay in scope within a setTimeout though.
@@ -172,7 +171,7 @@ export class MainScene extends DisplayObject{
 
         this.Particles = new ParticleLayer();
         this.DisplayList.Add(this.Particles);
-        this.Particles.Init(this, new Canvas());
+        this.Particles.Init(this);
 
         this._ToolTip = new ToolTip();
         this.DisplayList.Add(this._ToolTip);
