d4ab44de0839cb6053391886b0687b456787e0e9
Whitevinyl <neverest_songs@yahoo.co.uk>
2016-08-22 20:26:20 +0100

    Fixed Granular bug (loading new sample while powered)

--- app/Blocks/Sources/Sample.ts
+++ app/Blocks/Sources/Sample.ts
@@ -55,6 +55,8 @@ export class Sample extends SamplerBase {
 
         // Define Outline for HitTest
         this.Outline.push(new Point(-1, 0),new Point(0, -1),new Point(1, -1),new Point(2, 0),new Point(1, 1),new Point(0, 1));
+
+        //SoundCloudAPI.Monitor();
     }
 
     //-------------------------------------------------------------------------------------------
